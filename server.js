f2c20ffd7d75c0b0d2f42570501fdbeffb759904
jcastelain <jeremy.castelain@gpsa.fr>
2018-04-20 15:05:05 +0200

    index.html with new server.js

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
-my_http = require("http");
-my_http.createServer(function(request,response) {
-	console.log("received Get request");
-	response.writeHeader(200, {"Content-Type": "text/plain"});
-	response.write("Hello");
-	response.end();
-}).listen(8080);
-console.log("Server Running on 8080");
+var http = require('http');
+var fs = require('fs');
+
+// Chargement du fichier index.html affiché au client
+var server = http.createServer(function(req, res) {
+    fs.readFile('./index.html', 'utf-8', function(error, content) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(content);
+    });
+});
+
+// Chargement de socket.io
+var io = require('socket.io').listen(server);
+
+// Quand un client se connecte, on le note dans la console
+io.sockets.on('connection', function (socket) {
+    console.log('Un client est connecté !');
+});
+
+server.listen(8080);
